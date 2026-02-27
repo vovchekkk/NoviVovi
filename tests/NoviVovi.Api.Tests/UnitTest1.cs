112706477050423425d5154a2b8cf75181b7ad57
Vladimir Gavrikov <vlgavrikov06@gmail.com>
2026-02-27 15:27:30 +0500

    add tests

---
+++ tests/NoviVovi.Api.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace NoviVovi.Api.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/NoviVovi.Infrastructure.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace NoviVovi.Infrastructure.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
