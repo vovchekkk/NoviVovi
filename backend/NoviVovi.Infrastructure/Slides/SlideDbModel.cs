9b82a51e05f4af856dadf57d73b030c7eab78897
Vladimir Gavrikov <vlgavrikov06@gmail.com>
2026-03-06 20:35:47 +0500

    infrastucture concept

--- backend/NoviVovi.Infrastructure/Slides/SlideDbModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace NoviVovi.Infrastructure.Slides;
-
-public class SlideDbModel
-{
-    public int Number { get; set; }
-    public string Text { get; set; } = null!;
-    public Guid NovelId { get; set; }
-}
