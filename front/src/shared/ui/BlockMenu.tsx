2a660e815c7657920308fe132067ce8299243315
Алсу Исламова <161420770+mrrled@users.noreply.github.com>
2026-03-30 21:59:16 +0500

    editor page

--- front/src/shared/ui/BlockMenu.tsx
+++ front/src/shared/ui/BlockMenu.tsx
@@ -1,6 +1,4 @@
 import {css} from '../../../styled-system/css'
-import Selector from "./Selector.tsx";
-import MenuTextarea from "./MenuTextarea.tsx";
 
 interface BlockMenuProps {
     title: string;
@@ -15,21 +13,10 @@ export default function BlockMenu({title}: BlockMenuProps) {
             borderRadius:'12px',
         })}>
             <div className={css({
-                fontSize: '20px',
                 margin: '20px',
                 borderBottom: '1px solid black',
             })}>
                 {title}
-            </div>
-            <div className={css({
-                display: 'flex',
-                flexDirection: 'column',
-                gap:'20px',
-                alignItems: 'center',
-                justifyContent: 'center',
-            })}>
-                <Selector title={'Персонаж'} options={['fon', 'fonk', 'fenk']}></Selector>
-                <MenuTextarea title={'Текст'}></MenuTextarea>
             </div>
         </div>
     )
