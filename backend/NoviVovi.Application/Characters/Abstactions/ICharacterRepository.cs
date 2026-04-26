6a223172335af144dbae79a105d030f9f7715de9
Vladimir Gavrikov <vlgavrikov06@gmail.com>
2026-04-26 19:50:58 +0500

    methods for ICharacterRepository

--- backend/NoviVovi.Application/Characters/Abstactions/ICharacterRepository.cs
+++ backend/NoviVovi.Application/Characters/Abstactions/ICharacterRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace NoviVovi.Application.Characters.Abstactions;
+﻿using NoviVovi.Domain.Characters;
+
+namespace NoviVovi.Application.Characters.Abstactions;
 
 public interface ICharacterRepository
 {
-    
+    public Task<Character?> GetByIdAsync(Guid id, CancellationToken ct);
+    public Task AddOrUpdateAsync(Character character, CancellationToken ct);
+    public Task DeleteAsync(Character character, CancellationToken ct);
+    public Task<IEnumerable<Character>> GetAllAsync(CancellationToken ct);
 }
