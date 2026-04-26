45c60bcc5cbce4d88d424fb3cf5dbd7b47ad7c6c
MoskvinaAlexandra <Moskvina.Alexandra@urfu.me>
2026-04-26 21:25:54 +0500

    нужны айди

---
+++ backend/NoviVovi.Infrastructure/Repositories/CharacterRepository.cs
@@ -0,0 +1,34 @@
+using NoviVovi.Application.Characters.Abstactions;
+using NoviVovi.Domain.Characters;
+using NoviVovi.Infrastructure.Mappers;
+using NoviVovi.Infrastructure.Repositories.DbO.Interfaces;
+
+namespace NoviVovi.Infrastructure.Repositories;
+
+public class CharacterRepository(ICharacterDbORepository dboRepo, CharacterMapper mapper) : ICharacterRepository
+{
+    public async Task<Character?> GetByIdAsync(Guid id, CancellationToken ct)
+    {
+        var dbo = await dboRepo.GetFullCharacterByIdAsync(id);
+        if (dbo == null)
+            return null;
+        return mapper.ToDomain(dbo);
+    }
+
+    public async Task AddOrUpdateAsync(Character character, CancellationToken ct)
+    {
+        // var dbo = mapper.ToDbO(character);
+        // await dboRepo.AddOrUpdateFullAsync(dbo);
+        throw new NotImplementedException();
+    }
+
+    public async Task DeleteAsync(Character character, CancellationToken ct)
+    {
+        await dboRepo.DeleteAsync(character.Id);
+    }
+
+    public Task<IEnumerable<Character>> GetAllAsync(CancellationToken ct)
+    {
+        throw new NotImplementedException();
+    }
+}
