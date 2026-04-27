1749b6558147df32fc45cd7c2f0ff1acdc94696b
Vladimir Gavrikov <vlgavrikov06@gmail.com>
2026-04-27 22:34:20 +0500

    add polimorf serialization for steps

--- backend/NoviVovi.Api/Steps/Responses/StepResponse.cs
+++ backend/NoviVovi.Api/Steps/Responses/StepResponse.cs
@@ -1,7 +1,15 @@
-﻿using NoviVovi.Api.Transitions.Responses;
+﻿using System.Text.Json.Serialization;
+using NoviVovi.Api.Transitions.Responses;
 
 namespace NoviVovi.Api.Steps.Responses;
 
+[JsonPolymorphic(TypeDiscriminatorPropertyName = "type")]
+[JsonDerivedType(typeof(ShowReplicaStepResponse), "show_replica")]
+[JsonDerivedType(typeof(ShowCharacterStepResponse), "show_character")]
+[JsonDerivedType(typeof(HideCharacterStepResponse), "hide_character")]
+[JsonDerivedType(typeof(ShowBackgroundStepResponse), "show_background")]
+[JsonDerivedType(typeof(ShowMenuStepResponse), "show_menu")]
+[JsonDerivedType(typeof(JumpStepResponse), "jump")]
 public abstract record StepResponse
 {
     public required Guid Id { get; init; }
--- backend/NoviVovi.Application/Steps/Dtos/StepDto.cs
+++ backend/NoviVovi.Application/Steps/Dtos/StepDto.cs
@@ -1,7 +1,15 @@
-﻿using NoviVovi.Application.Transitions.Dtos;
+﻿using System.Text.Json.Serialization;
+using NoviVovi.Application.Transitions.Dtos;
 
 namespace NoviVovi.Application.Steps.Dtos;
 
+[JsonPolymorphic(TypeDiscriminatorPropertyName = "type")]
+[JsonDerivedType(typeof(ShowReplicaStepDto), "show_replica")]
+[JsonDerivedType(typeof(ShowCharacterStepDto), "show_character")]
+[JsonDerivedType(typeof(HideCharacterStepDto), "hide_character")]
+[JsonDerivedType(typeof(ShowBackgroundStepDto), "show_background")]
+[JsonDerivedType(typeof(ShowMenuStepDto), "show_menu")]
+[JsonDerivedType(typeof(JumpStepDto), "jump")]
 public abstract record StepDto
 {
     public required Guid Id { get; init; }
